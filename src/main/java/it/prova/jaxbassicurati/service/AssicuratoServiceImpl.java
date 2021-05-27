c3dbb016db62648ba30310b824f4d191ee42b281
Solving-Enrico <cristofanillienrico@gmail.com>
2021-05-27 15:07:29 +0200

    finito

--- src/main/java/it/prova/jaxbassicurati/service/AssicuratoServiceImpl.java
+++ src/main/java/it/prova/jaxbassicurati/service/AssicuratoServiceImpl.java
@@ -2,6 +2,7 @@ package it.prova.jaxbassicurati.service;
 
 import it.prova.jaxbassicurati.model.Assicurato;
 import it.prova.jaxbassicurati.repository.AssicuratoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.PersistenceContext;
@@ -10,7 +11,8 @@ import java.util.List;
 
 @Service
 public class AssicuratoServiceImpl implements AssicuratoService {
-    @PersistenceContext
+
+    @Autowired
     private AssicuratoRepository repository;
 
     @Override
